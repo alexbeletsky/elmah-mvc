aaa1274173a82f5f99b76d5a6169b4a4dc43273b
alexander.beletsky <alexander.beletsky@gmail.com>
2011-03-23 09:23:42 +0200

    initial commit

---
+++ Controllers/ElmahController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Areas.Admin.Controllers
+{
+    class ElmahResult : ActionResult
+    {
+        private string _resouceType;
+
+        public ElmahResult(string resouceType)
+        {
+            _resouceType = resouceType;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var factory = new Elmah.ErrorLogPageFactory();
+            if (!string.IsNullOrEmpty(_resouceType))
+            {
+                var pathInfo = "." + _resouceType;
+                HttpContext.Current.RewritePath(HttpContext.Current.Request.Path, pathInfo, HttpContext.Current.Request.QueryString.ToString());
+            }
+
+            var handler = factory.GetHandler(HttpContext.Current, null, null, null);
+
+            handler.ProcessRequest(HttpContext.Current);
+        }
+    }
+
+    public class ElmahController : Controller
+    {
+        public ActionResult Index(string type)
+        {
+            return new ElmahResult(type);
+        }
+
+        public ActionResult Detail(string type)
+        {
+            return new ElmahResult(type);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Elmah MVC Controller
+====================
+
+This is source code for controller to integrate ELMAH into MVC appilcation.
+
+See, http://www.beletsky.net/2011/03/integrating-elmah-to-aspnet-mvc-in.html
