37a133cd9580cee3369693b2f89e04944f4d8fc3
alexander.beletsky <alexander.beletsky@gmail.com>
2011-09-06 21:30:33 +0300

    corrected package to include area registration

---
+++ nuget/content/Areas/Admin/AdminAreaRegistration.cs.pp
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace $rootnamespace$.Areas.Admin {
+    public class AdminAreaRegistration : AreaRegistration {
+        public override string AreaName {
+            get {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
