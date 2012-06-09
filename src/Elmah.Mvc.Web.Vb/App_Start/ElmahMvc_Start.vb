1b62b7a93faa20a41de452910733f1c7ff5a68e1
alexander.beletsky <alexander.beletsky@gmail.com>
2012-06-09 16:21:54 +0300

    VB stuff

---
+++ src/Elmah.Mvc.Web.Vb/App_Start/ElmahMvc_Start.vb
@@ -0,0 +1,8 @@
+﻿'<Assembly: WebActivator.PreApplicationStartMethod(Type.GetType("ElmahMvc_Start"), "Start")> 
+
+'Public Class ElmahMvc_Start
+'    Public Shared Sub ElmahMvc_Start()
+'        Elmah.Mvc.Bootstrap.Initialize()
+'    End Sub
+'End Class
+
