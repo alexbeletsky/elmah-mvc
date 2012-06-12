d7f8d1f4bed5f56b2d656b51b49f63d4cecac99a
alexander.beletsky <alexander.beletsky@gmail.com>
2012-06-13 00:18:25 +0300

    corrected assembly info

--- src/Elmah.Mvc/Properties/AssemblyInfo.cs
+++ src/Elmah.Mvc/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Elmah.Mvc")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ELMAH adapter to ASP.NET MVC applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Elmah.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Atif Aziz, James Driscoll, Alexander Beletsky 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.*")]
+[assembly: AssemblyFileVersion("2.0.0")]
