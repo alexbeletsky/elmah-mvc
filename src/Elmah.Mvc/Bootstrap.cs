16a7833372a2b1e5c41061f0039e554ade3dd4eb
Chao <chaoaretasty@gmail.com>
2013-05-24 12:57:43 +0200

    Fixed comparison on route

--- src/Elmah.Mvc/Bootstrap.cs
+++ src/Elmah.Mvc/Bootstrap.cs
@@ -63,7 +63,7 @@ namespace Elmah.Mvc
                 null,
                 namespaces);
 
-            if (elmahRoute == "elmah" && Settings.IgnoreDefaultRoute)
+            if (elmahRoute != "elmah" && Settings.IgnoreDefaultRoute)
             {
                 routes.IgnoreRoute("elmah");
                 routes.IgnoreRoute("elmah/{*pathinfo}");
