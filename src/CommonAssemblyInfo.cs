6d16c5d6385cb15af45f59f6121bcebf41cb3e80
issafram <issafram@gmail.com>
2015-04-27 20:51:53 -0400

    Add CommonAssemblyInfo class. Increase version to 2.1.2

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Elmah.Mvc")]
+[assembly: AssemblyCopyright("Copyright © Atif Aziz, James Driscoll, Alexander Beletsky 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("2.1.2.*")]
+[assembly: AssemblyFileVersion("2.1.2")]
+[assembly: AssemblyInformationalVersion("2.1.2")]
