8775c11c3beee520e1f757ab8452a7c3286803b3
issafram <issafram@gmail.com>
2015-05-11 23:01:11 -0400

    Add unit test for ActionEqualsDetail.

--- src/Tests/ElmahResultTests.cs
+++ src/Tests/ElmahResultTests.cs
@@ -1,7 +1,9 @@
 namespace Elmah.Mvc.Tests
 {
+    using System.Collections.Specialized;
     using System.Web;
     using System.Web.Mvc;
+    using System.Web.Routing;
 
     using Moq;
 
@@ -42,6 +44,27 @@ namespace Elmah.Mvc.Tests
             httpContextBase.Setup(x => x.Request).Returns(request.Object);
             var controllerContext = new Mock<ControllerContext>();
             controllerContext.Setup(x => x.RouteData).Returns(() => null);
+            controllerContext.Setup(x => x.HttpContext).Returns(httpContextBase.Object);
+            var elmahResult = new ElmahResult();
+
+            // Assert
+            Assert.DoesNotThrow(() => elmahResult.ExecuteResult(controllerContext.Object));
+        }
+
+        [Fact]
+        public void ElmahResult_ExecuteResult_ResourceNullActionEqualsDetail()
+        {
+            // Arrange
+            var request = new Mock<HttpRequestBase>();
+            request.Setup(x => x.HttpMethod).Returns("GET");
+            request.Setup(x => x.Path).Returns(@"elmah/detail/detail/stylesheet");
+            request.Setup(x => x.QueryString).Returns(new NameValueCollection());
+            var httpContextBase = new Mock<HttpContextBase>();
+            httpContextBase.Setup(x => x.Request).Returns(request.Object);
+            var routeData = new RouteData();
+            routeData.Values.Add("action", "Detail");
+            var controllerContext = new Mock<ControllerContext>();
+            controllerContext.Setup(x => x.RouteData).Returns(routeData);
             controllerContext.Setup(x => x.HttpContext).Returns(httpContextBase.Object);
             var elmahResult = new ElmahResult();
 
